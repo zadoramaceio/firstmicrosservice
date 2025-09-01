3ba17af30835c85fbca85f8fe6652dc80cbce88c
zadoramaceio <dora13.isa@gmail.com>
2025-09-01 17:52:33 -0300

    implementation endpoint

---
+++ hr-worker/src/main/java/com/example/devsuperior/hrworker/resources/WorkerResource.java
@@ -0,0 +1,36 @@
+package com.example.devsuperior.hrworker.resources;
+
+import com.example.devsuperior.hrworker.entities.Worker;
+import com.example.devsuperior.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerRepository repository;
+
+    public WorkerResource(WorkerRepository repository) {
+        this.repository = repository;
+    }
+        @GetMapping
+        public ResponseEntity<List<Worker>> findAll(){
+            List<Worker> list = repository.findAll();
+
+            return ResponseEntity.ok(list);
+        }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        Worker byId = repository.findById(id).get();
+
+        return ResponseEntity.ok(byId);
+    }
+}
